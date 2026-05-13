9da2e3325e96f45e665e006c436d6f80641381ca
Armando <zavaarmando0@gmail.com>
2026-05-12 21:11:09 -0400

    Delete dist directory

--- dist/api/readiness.js
+++
@@ -1,4 +0,0 @@
-export function handlerReadiness(req, res) {
-    res.set('Content-Type', 'text/plain');
-    res.send("OK");
-}
--- dist/client.js
+++
@@ -1,6 +0,0 @@
-import postgres from "postgres";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
-import { drizzle } from "drizzle-orm/postgres-js";
-import { config } from "./config";
-const migrationClient = postgres(config.dbConfig.dbURL, { max: 1 });
-await migrate(drizzle(migrationClient), config.dbConfig.config);
